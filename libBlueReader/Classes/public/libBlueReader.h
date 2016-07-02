836b8fc55dc40df76f4f570d38176a2da9a752d4
Sandra Keßler <sk@softwarehaus-kassel.de>
2016-07-02 19:13:56 +0200

    add public header

---
+++ libBlueReader/Classes/public/libBlueReader.h
@@ -0,0 +1,15 @@
+//
+//  libBlueReader.h
+//
+//  BlueReader Project
+//  http://bluereader.de
+//
+//  Created by Sandra Keßler on 02.07.2016
+//  Copyright © 2016 blueToolz UG. All rights reserved.
+//
+
+#ifndef libBlueReader_h
+#define libBlueReader_h
+
+
+#endif /* libBlueReader_h */
